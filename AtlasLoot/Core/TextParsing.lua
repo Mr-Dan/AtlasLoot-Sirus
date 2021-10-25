79a7d9ea291462258cae1da3d38c7d192218c601
Mr-Dan <35195104+Mr-Dan@users.noreply.github.com>
2021-10-25 14:51:29 +0300

    Add files via upload

--- AtlasLoot/Core/TextParsing.lua
+++ AtlasLoot/Core/TextParsing.lua
@@ -1104,6 +1104,7 @@ function AtlasLoot_FixText(text)
 	text = gsub(text, "#pigment#", BabbleInventory["pigment"]);--Полироль 
 	text = gsub(text, "#blackpigment#", BabbleInventory["blackpigment"]);--Полироль 
 	text = gsub(text, "#Tol'Garod#", "|TInterface\\Icons\\Achievement_Zone_TolBarad:15|t");--жетоны Тол'Гарод
+	text = gsub(text, "#Coin of Ancestry#", "|TInterface\\Icons\\inv_misc_elvencoins:15|t");-- Монета Наследия
 
 	text = gsub(text, "#valentineday#", "|TInterface\\Icons\\inv_valentinescard01:0|t");
 	text = gsub(text, "#valentineday2#", "|TInterface\\Icons\\inv_jewelry_necklace_43:0|t");
