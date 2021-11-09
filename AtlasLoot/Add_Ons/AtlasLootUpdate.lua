3735f1d231f25417925869c0fca02b1b32bd36a2
Mr-Dan <35195104+Mr-Dan@users.noreply.github.com>
2021-11-09 20:58:38 +0300

    Add files via upload

--- AtlasLoot/Add_Ons/AtlasLootUpdate.lua
+++ AtlasLoot/Add_Ons/AtlasLootUpdate.lua
@@ -10,8 +10,7 @@ frame:SetScript("OnEvent", function(frame, event, ...)
  if (event == "PLAYER_ENTERING_WORLD") then
 	if (AtlasLootNeedUprate~= nil) then
 		if (tonumber(AtlasLootNeedUprate) > current_version) then
-			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])
-			print (AL["Need Update"])
+			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])			
 		end
 	end
 		SendAddonMessage("AtlasLootUpdater", tostring(current_version), "GUILD")		
@@ -19,8 +18,7 @@ frame:SetScript("OnEvent", function(frame, event, ...)
 elseif (event == "CHAT_MSG_ADDON" and arg1 == "AtlasLootUpdater" and arg3 == "GUILD") then
 
 		if (tonumber(arg2) > current_version) then
-			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])
-			print (AL["Need Update"])
+			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])			
 			AtlasLootNeedUprate = arg2
 		end
 	end
