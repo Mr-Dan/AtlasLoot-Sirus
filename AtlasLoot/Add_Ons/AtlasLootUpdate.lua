223a7a72e4d7856f86bb3f1533316bcb66f5bd8a
fxpw <fxpw@outlook.com>
2021-11-26 19:04:30 +0300

    fix last

--- AtlasLoot/Add_Ons/AtlasLootUpdate.lua
+++ AtlasLoot/Add_Ons/AtlasLootUpdate.lua
@@ -10,10 +10,10 @@ frame:SetScript("OnEvent", function(frame, event, ...)
  if (event == "PLAYER_ENTERING_WORLD") then
 	if (AtlasLootNeedUprate~= nil) then
 		if (tonumber(AtlasLootNeedUprate) > tonumber(current_version)) then
-			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])			
+			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])
 		end
 	end
-	
+
 	if GetNumRaidMembers() > 1 then
 		local _, instanceType = IsInInstance()
 		if instanceType == "pvp" then
@@ -26,11 +26,11 @@ frame:SetScript("OnEvent", function(frame, event, ...)
 	elseif IsInGuild() then
         SendAddonMessage("AtlasLootUpdater", tostring(current_version), "GUILD")
     end
-			
+
 elseif (event == "CHAT_MSG_ADDON" and arg1 == "AtlasLootUpdater" ) then
 
 		if (tonumber(arg2) > tonumber(current_version)) then
-			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])			
+			getglobal("AtlasLootDefaultFrame_Notice"):SetText(AL["Need Update"])
 			AtlasLootNeedUprate = arg2
 		end
 	end
