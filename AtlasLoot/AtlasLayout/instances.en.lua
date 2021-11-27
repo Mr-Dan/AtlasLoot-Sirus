f26dd15c5a4a215a1b5658ecb7dad7d186f6284e
Mr-Dan <35195104+Mr-Dan@users.noreply.github.com>
2021-11-27 03:20:33 +0300

    for atlas

--- AtlasLoot/AtlasLayout/instances.en.lua
+++ AtlasLoot/AtlasLayout/instances.en.lua
@@ -9,12 +9,12 @@ AtlasLootNewBossButtons = {
   ---------------------
    BronzeSanctuary = {
 		"";
-        "";
-        "Imporus25Man";
-        "Elonus25Man";
-        "Murozond25Man";
 		"";
-        "";
+		"Imporus25Man";
+		"Elonus25Man";
+		"Murozond25Man";
+		"";
+		"";
 		"";
 		"";
 		"GrainOfSandOfLost";
